04fbbddeeb728ca3a5ffd434b1ab3b0a131c41b0
Andre Motta <iandremotta@gmail.com>
2020-07-03 20:02:10 -0300

    Small fix

---
+++ views/permissions.php
@@ -0,0 +1,15 @@
+<h1>Permissões</h1>
+
+<div class="tabarea">
+    <div class="tabitem activetab">Grupos de permissões</div>
+    <div class="tabitem">Permissões</div>
+</div>
+
+<div class="tabcontent">
+    <div class="tabbody">
+        Grupo de permissões
+    </div>
+    <div class="tabbody">
+        Permissões
+    </div>
+</div>
--- views/template.php
+++ views/template.php
@@ -3,13 +3,22 @@
 <head>
     <title>Painel - <?php echo $viewData['company_name']; ?></title>
     <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>/assets/css/style.css">
-    <link rel="stylesheet" href="<?php BASE_URL; ?>assets/css/template.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/css/template.css">
+    <script src='<?php echo BASE_URL; ?>/assets/js/jquery-3.5.1.min.js'></script>
+    <script src='<?php echo BASE_URL; ?>/assets/js/script.js'></script>
 </head>
 
 <body>
     <div class="leftmenu">
         <div class="company_name">
             <?php echo $viewData['company_name']; ?>
+        </div>
+
+        <div class="menuarea">
+            <ul>
+                <li> <a href="<?php echo BASE_URL; ?>"> Home</a></li>
+                <li> <a href="<?php echo BASE_URL; ?>/permissions"> Permissões</a></li>
+            </ul>
         </div>
     </div>
 
@@ -19,11 +28,13 @@
             <div class="top_right">
                 <?php echo $viewData['user_email']; ?>
             </div>
-
+        </div>
+        <div class="area">
+            <?php $this->loadViewIntemplate($viewName, $viewData); ?>
         </div>
     </div>
 
-    <!-- <?php $this->loadViewIntemplate($viewName, $viewData); ?> -->
+
 </body>
 
 </html>
